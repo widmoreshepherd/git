86b732f26b360c4fcc94026802a25485b0266e5b
widmore <lt22390@126.com>
2016-08-27 11:22:04 +0800

    A13

--- app/src/main/java/com/example/luotong/git/MainActivity.java
+++ app/src/main/java/com/example/luotong/git/MainActivity.java
@@ -13,28 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = (TextView)findViewById(R.id.textView);
+        textView = (TextView) findViewById(R.id.textView);
         textView.setText("123");
         //原版
-        add(9,10);
-        test(5,6,7);
-        show1("title");
-    }
-
-    private int add(int a, int b){
-        return test(a,b,0);
-    }
-
-    private int test(int a,int b,int c){
-        return a + b + c;
-    }
-
-    private void show1(String message){
-        Toast.makeText(this,message, Toast.LENGTH_SHORT).show();
-    }
-
-    private void show2(){
-
     }
 
 }
