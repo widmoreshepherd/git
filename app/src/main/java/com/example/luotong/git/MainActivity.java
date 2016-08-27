368284c994a7caaad34b4aff699d1324f7c4d748
widmore <lt22390@126.com>
2016-08-27 09:48:42 +0800

    A1

--- app/src/main/java/com/example/luotong/git/MainActivity.java
+++ app/src/main/java/com/example/luotong/git/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         textView = (TextView)findViewById(R.id.textView);
         textView.setText("123");
+        //
     }
 }
